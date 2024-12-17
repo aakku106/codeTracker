b9aaef6a4d01dfbdc289bdc87a35342f8b0c41d9
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2024-12-17 18:26:36 +0000

    Auto-update logs at 2024-12-17 18:26:36

--- update_repo.py
+++ update_repo.py
@@ -5,15 +5,24 @@ from datetime import datetime
 # GitHub Repo Commit and Push Automation
 def git_push():
     try:
-        # Add all changes to git (stages all modified files)
-        # check 3
-        subprocess.run(["git", "add", "."], check=True)
+        # Pull the latest changes from GitHub(changed from my mac😁)
+        # this creates 30 min latency in actually updating website
+        # when midifing things from my mac(or any other devide locally)
+        # if want to implement fast(remove 30 min latency), one shall update from server
+        subprocess.run(["git", "pull", "origin", "main"], check=True)
 
+        # Add changes to git
+        # subprocess.run(["git", "add", "logs.csv", "README.md"], check=True)
+        # above add ws to update table, but creates problem while updating logic for server so
+        # until the server is totally fixed for 60 days use below add all
+
+        # Add all changes to git (stages all modified files)
+        subprocess.run(["git", "add", "."], check=True)
         # Commit the changes with a timestamp message
         commit_message = f"Auto-update logs at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
         subprocess.run(["git", "commit", "-m", commit_message], check=True)
 
-        # Push the changes (Git will use cached credentials here)
+        # Push the changes
         subprocess.run(["git", "push"], check=True)
 
         print("✅ Changes pushed to GitHub successfully!")
